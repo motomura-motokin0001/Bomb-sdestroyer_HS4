d842749ac1fe4eb2a83c0fcd6039802fd40c394d
本村煌雅 <human.kagoshima.kouga@gmail.com>
2024-07-31 10:03:28 +0900

    ゲームを閉じることが可能に

---
+++ Bomb's Destroyer/Assets/スクリプト/ExitGame.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    void Update()
+    {
+        // Escapeキーが押された場合
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // エディタで実行中の場合は停止
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            // ビルドされたゲームの場合はアプリケーションを終了
+            Application.Quit();
+            #endif
+        }
+    }
+}
